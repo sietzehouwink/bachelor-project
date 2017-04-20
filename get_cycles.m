cecb252c5208d99fc52b19d3c564c28f4ea2f683
Sietze Houwink <sietzehouwink@gmail.com>
2017-04-20 19:09:26 +0200

    Refactored and improved cycle generation algorithm.

--- get_cycles.m
+++ get_cycles.m
@@ -1,43 +1,38 @@
-% Assumes connected components, starts from first vertex!!!!!!!
 function [cycles] = get_cycles(graph)
-    stack = [];
-    stack = push(stack, 1);
-    parents_of_edges = zeros(graph.nr_vertices,1);
+    stack = (1:graph.nr_vertices)';
+    parents = zeros(graph.nr_vertices,1);
     discovered = false(graph.nr_vertices,1);
-    cycles = cell(0,0);
+    cycles = cell(0);
     while ~isempty(stack) 
-        [stack, vertex] = pop(stack);
-        if ~discovered(vertex)
-            discovered(vertex) = true;
-            adjacent_vertices = graph.adj_list{vertex};
-            nr_outgoing_edges = size(adjacent_vertices,1);
-            for i = 1:nr_outgoing_edges
-                if ~discovered(adjacent_vertices(i))
-                    stack = push(stack, adjacent_vertices(i));
-                    parents_of_edges(adjacent_vertices(i)) = vertex;
+        [stack, visiting_vertex] = pop(stack);
+        if ~discovered(visiting_vertex,1)
+            discovered(visiting_vertex,1) = true;
+            for adjacent_vertex = graph.adj_list{visiting_vertex,1}'
+                if discovered(adjacent_vertex)
+                    cycles{end+1,1} = infer_cycle(parents, adjacent_vertex, visiting_vertex);
                 else
-                    cycles{end+1,1} = get_path(parents_of_edges, vertex, adjacent_vertices(i));
+                    stack = push(stack, adjacent_vertex);
+                    parents(adjacent_vertex,1) = visiting_vertex;
                 end
             end
         end
     end
 end
 
-function [path] = get_path(parents, last, first)
-    path = [];
-    while last ~= first
-        path = [last; path];
-        last = parents(last);
+function [cycle] = infer_cycle(parents, first_vertex, last_vertex)
+    cycle = [];
+    while last_vertex ~= first_vertex
+        cycle = [last_vertex; cycle];
+        last_vertex = parents(last_vertex);
     end
-    path = [last; path];
+    cycle = [last_vertex; cycle];
 end
 
-function [stack] = push(stack, element)
-    stack = [stack; element];
+function [stack] = push(stack, value)
+    stack = [stack; value];
 end
       
 function [stack, value] = pop(stack)
-    nr_elements = size(stack,1);
-    value = stack(nr_elements);
-    stack = stack(1:end-1);
+    value = stack(end,1);
+    stack = stack(1:end-1,1);
 end
