838fd6272c63e18fd085f8ed3cff03327bf8df5e
Sietze Houwink <sietzehouwink@gmail.com>
2017-04-23 17:51:14 +0200

    Upgraded test suite to test on pre-generated graphs of different types.

---
+++ get_test_graphs.m
@@ -0,0 +1,32 @@
+function [graphs, names] = get_test_graphs()
+
+    fileID = fopen('graphs.txt');
+
+    excluded = {'Balaban10Cage', 'Balaban11Cage', 'BarnetteBosakLederbergGraph', 'BiggsSmithGraph', 'BipartiteKneser', 'BrinkmannGraph'};
+
+    graphs = {};
+    names = {};
+
+    for i = 1:144
+        line = fgets(fileID);
+        indices = regexp(line,'"');
+        name = line(indices(1)+1:indices(2)-1);
+        if ~isempty(strmatch(name, excluded))
+            continue;
+        else
+            excluded{end+1} = name; % Exclude further of same type.
+            names{end+1} = name;
+        end
+        indices = regexp(line,'{{');
+        line = line(indices(end):end);
+        line = strrep(line,'{','');
+        line = strrep(line,'}','');
+        line = strrep(line,',','');
+        [A,n] = sscanf(line,'%d');
+        A = reshape(A,2,n/2);
+        graph = digraph(A(1,:), A(2,:));
+        graphs{end+1} = graph;
+    end
+
+end
+
--- test_suite.m
+++ test_suite.m
@@ -1,34 +1,36 @@
 clear global;
 clear variables;
 
-nr_vertices_test_set = (100:100:500)';
-execution_times_edge_formulation = zeros(size(nr_vertices_test_set));
-execution_times_cycle_formulation = zeros(size(nr_vertices_test_set));
+[test_graphs,names] = get_test_graphs;
+nr_graphs = length(test_graphs);
 
-for index = 1:size(nr_vertices_test_set,1)
-    nr_vertices = nr_vertices_test_set(index);
-    graph = generate_graph(nr_vertices, nr_vertices / 10, 5);
+execution_times_edge_formulation = zeros(nr_graphs,1);
+execution_times_cycle_formulation = zeros(nr_graphs,1);
+
+for graph_index = 1:length(test_graphs) 
+    
+    graph = test_graphs{graph_index};
     
     tic;
     [~,value] = edge_formulation_solver(graph);
     exec_time = toc;
-    execution_times_edge_formulation(index) = exec_time;
-    fprintf('Edge formulation: nr_vertices: %d, value: %d, fraction: %f, exec_time: %f\n', nr_vertices, value, value/nr_vertices, exec_time);
+    execution_times_edge_formulation(graph_index) = exec_time;
+    fprintf('Edge formulation: value: %d, exec_time: %f\n', value, exec_time);
     
     tic; % Also takes cycle generation into account.
     [~,value] = cycle_formulation_solver(graph);
     exec_time = toc;
-    execution_times_cycle_formulation(index) = exec_time;
-    fprintf('Cycle formulation: nr_vertices: %d, value: %d, fraction: %f, exec_time: %f\n', nr_vertices, value, value/nr_vertices, exec_time);
+    execution_times_cycle_formulation(graph_index) = exec_time;
+    fprintf('Cycle formulation: value: %d, exec_time: %f\n', value, exec_time);
     
 end
 
-figure('Name','Execution time vs. number of vertices.');
+figure('Name','Edge / cycle formulation performance.');
 hold on;
-plot(nr_vertices_test_set, execution_times_edge_formulation, 'Marker', '*');
-plot(nr_vertices_test_set, execution_times_cycle_formulation, 'Marker', '*');
-title('Execution time vs. number of vertices.');
-xlabel('Number of vertices');
+set(gca,'yTick', 1:nr_graphs,'yticklabel',names);
+barh([execution_times_edge_formulation execution_times_cycle_formulation]);
+title('Edge / cycle formulation performance.');
+xlabel('Execution time (s)');
+ylabel('Graph type');
 legend('Edge formulation', 'Cycle formulation');
 hold off;
-ylabel('Execution time (s)');
