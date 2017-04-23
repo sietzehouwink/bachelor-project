39d2140100a30360b0e0fa8189b335d66eef259f
Sietze Houwink <sietzehouwink@gmail.com>
2017-04-23 11:00:31 +0200

    Removed to_matlab_graph.

--- to_matlab_graph.m
+++
@@ -1,9 +0,0 @@
-function [matlab_graph] = to_matlab_graph(own_graph)
-    adj_matrix = zeros(own_graph.nr_vertices, own_graph.nr_vertices);
-    for i = 1:own_graph.nr_vertices
-        list = own_graph.adj_list{i};
-        adj_matrix(i,list) = 1;
-    end
-    matlab_graph = digraph(adj_matrix);
-end
-
