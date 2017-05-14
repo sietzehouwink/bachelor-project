d0cb90cde7bfbf9d1b2dca4421c2eb0206fd5d05
Sietze Houwink <sietzehouwink@gmail.com>
2017-05-14 16:06:51 +0200

    Added test function for bipartite formulation solver.

---
+++ test_bipartite_formulation_solver.m
@@ -0,0 +1,19 @@
+clear global;
+clear variables;
+load('graph_article_glorie', 'graph');
+
+profile on
+[activated_graph, max_exchange_value, timed_out] = bipartite_formulation_solver(graph, 1);
+profile viewer
+
+if timed_out
+    disp('timed_out');
+else
+    disp('### Cleared market. ###');
+    fprintf('Maximum exchange value: %d\n', max_exchange_value);
+    fprintf('Activated edges: ');
+    for edge = table2array(activated_graph.Edges(:,1))'
+        fprintf('[%d %d] ', edge(1,1), edge(2,1));
+    end
+    fprintf('\n');
+end
