c3fbe2532690da16786a49f07d1f9de0b78c06dd
Sietze Houwink <sietzehouwink@gmail.com>
2017-06-01 13:28:41 +0200

    Removal of old files in git repo.

--- digraph_tools/get_node_containment_count_matrix.m
+++
@@ -1,6 +0,0 @@
-function [node_containment_count_matrix] = get_node_containment_count_matrix(cycles_chains, nr_nodes)
-    node_containment_count_matrix = zeros(nr_nodes, length(cycles_chains));
-    for cycle_index = 1:length(cycles_chains)
-        node_containment_count_matrix(cycles_chains{cycle_index}, cycle_index) = 1;
-    end
-end
--- unrestricted_clearing_problem/cycle_formulation/unrestricted_cycles_chains_solver.m
+++
@@ -1,4 +0,0 @@
-function [activated_digraph, exchange_value, timed_out, core_exec_time] = unrestricted_cycles_chains_solver(digraph, timeout_find_cycles_chains, timeout_solver, optimoptions)
-    [activated_digraph, exchange_value, timed_out, core_exec_time] = restricted_cycles_chains_solver(digraph, Inf, Inf, timeout_find_cycles_chains, timeout_solver, optimoptions);
-end
-
